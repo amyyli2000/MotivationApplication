40ea89586f4e722a724ddfeb500aff287fb3f299
Laurent Lee <laurentl1212@gmail.com>
2019-04-06 18:00:02 -0700

    Edited the date

--- app/src/main/java/com/example/motivationapplication/MainActivity.java
+++ app/src/main/java/com/example/motivationapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.motivationapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://www.youtube.com/watch?v=G6AkbQkfywE
+        Thread thread = new Thread() {
+            @Override
+            public void run(){
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run(){
+                                TextView dateText = (TextView) findViewById(R.id.dateText);
+                                long date = System.currentTimeMillis();
+                                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd yyyy\nhh-mm-ss a");
+                                String dateString = sdf.format(date);
+                                dateText.setText(dateString);
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+        thread.start();
     }
 }
